79a2f224159e5f46d337abae7e6a2347b3349fa9
suheyb becerek <suheybbecerek@gmail.com>
2022-12-05 03:35:10 +0100

    cmake version with CPP

---
+++ cmake/pi-search/pi-search.cpp
@@ -0,0 +1,16 @@
+﻿// pi-search.cpp : Defines the entry point for the application.
+//
+#include "generate-pi.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        cout << "Please specify up to which place Pi should be calculated" << endl;
+        return EXIT_FAILURE;
+    }
+    int place = atoi(argv[1]);
+    return Chudnovsky(place);
+}
