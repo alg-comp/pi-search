295e7600b79516b4f255aa6681d6fecc6bd3d1eb
suheyb becerek <suheybbecerek@gmail.com>
2022-12-05 01:07:33 +0100

    initial cpp

---
+++ cpp/pi-search/pi-search/pi-search.cpp
@@ -0,0 +1,17 @@
+﻿// pi-search.cpp : Defines the entry point for the application.
+//
+#include "generate-pi.cpp"
+
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        cout << "Please specify up to which place Pi should be calculated";
+        return -1;
+    }
+    int place = atoi(argv[1]);
+    return Chudnovsky(place);
+}
